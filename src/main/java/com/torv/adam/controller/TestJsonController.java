28119f8f88ac37267cdc2c56821b224c9697c8c2
jianelegant <jian.elegant@gmail.com>
2016-10-14 20:29:31 +0800

    add query string type request test

--- src/main/java/com/torv/adam/controller/TestJsonController.java
+++ src/main/java/com/torv/adam/controller/TestJsonController.java
@@ -1,5 +1,6 @@
 package com.torv.adam.controller;
 
+import net.sf.json.JSONObject;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -9,11 +10,27 @@ import org.springframework.web.bind.annotation.ResponseBody;
  * Created by AdamLi on 2016/10/14.
  */
 @Controller
+@RequestMapping("/json")
 public class TestJsonController {
 
-    @RequestMapping(value = "/testjson", method = RequestMethod.GET)
+    @RequestMapping(value = "/get", method = RequestMethod.GET)
     @ResponseBody
-    public String getJson() {
+    public String get() {
         return "{\"name\":\"Adam\", \"age\":29}";
+    }
+
+    /**
+     * http://localhost:8080/json/query?userid="1"&username="adam"
+     * @param userid
+     * @param username
+     * @return
+     */
+    @RequestMapping(value = "/query")
+    @ResponseBody
+    public String query(String userid, String username) {
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("userid", userid);
+        jsonObject.put("username", username);
+        return jsonObject.toString();
     }
 }
